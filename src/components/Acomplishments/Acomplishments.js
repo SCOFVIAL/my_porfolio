9808c226b3a5b20c339ab53e746bfe658319f359
VICTOR ALLIU <alliuvictor@gmail.com>
2023-08-02 22:21:35 +0100

    Files updated

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,10 +4,10 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 5, text: 'Open Source Projects'},
+  { number: 1, text: 'Hackhathon Participated In', },
+  { number: 10, text: 'Github Projects', },
+  { number: 20, text: 'Github Commits', }
 ];
 
 const Acomplishments = () => (
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,33 +1,5 @@
 import styled from 'styled-components';
 export const projects = [
-  // {
-  //   title: 'Microsoft Packages',
-  //     description: "Highly proficient in Microsoft packages, with extensive hands-on experience and unwavering dedication to staying updated. I possess a deep understanding and mastery of diverse functionalities, enhancing software utilization.",  
-  //   // description: "Proficiently skilled in the comprehensive suite of Microsoft packages, encompassing an extensive range of software tools and applications. With a wealth of hands-on experience and an unwavering dedication to staying up-to-date with the latest advancements, I have developed a deep understanding and mastery of the diverse functionalities and capabilities offered by Microsoft's software offerings.",
-  //     image: '/images/mp1.png',
-  //     tags: ['Excel', 'Powerpoint', 'Word'],
-  //   source: 'https://google.com',
-  //   visit: 'https://google.com',
-  //   id: 0,
-  // },
-  // {
-  //   title: 'System Administrator',
-  //   description:"With extensive system administration expertise and a passion for optimizing computer systems, I possess hands-on experience in installing, configuring, and maintaining operating systems for seamless functionality and robust security.",
-  //   image: '/images/2.png',
-  //   tags: ['Microsoft Windows', 'MacOS', 'Linux in all Distros'],
-  //   source: 'https://google.com',
-  //   visit: 'https://google.com',
-  //   id: 1,
-  // },
-  // {
-  //   title: 'Colaboration',
-  //   description: "my proficiency in problem-solving, critical thinking, teamwork, and innovation empowers me to navigate complexities with agility, devise optimal solutions, collaborate effectively, and continuously push the boundaries of what is possible. These skills form the cornerstone of my approach, enabling me to deliver exceptional results and make a meaningful impact in any endeavor I undertake.",
-  //     image: '/images/3.jpg',
-  //     tags: ['React', 'WebRTC'],
-  //   source: 'https://google.com',
-  //   visit: 'https://google.com',
-  //   id: 2,
-  // },
   {
     title: 'Microsoft Packages',
     description: "Skilled in Microsoft packages, with hands-on experience and dedication to staying updated. Deep understanding and mastery for enhanced software utilization.",
@@ -48,7 +20,6 @@ export const projects = [
   },
   {
     title: 'Web Development',
-    // description: "Proficient in problem-solving, critical thinking, teamwork, and innovation, enabling agile navigation of complexities and delivery of exceptional results with a meaningful impact",
     description: "I excel in crafting scalable websites using cutting-edge tech, hands-on expertise, load balancing, cloud integration, and continuous optimization.",
     image: '/images/My project (7).png',
     tags: ['HTML 5', 'CSS 3', 'Node Js'],
@@ -68,9 +39,8 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2020, text: 'Started my journey', },
+  { year: 2021, text: 'Got my first certificate in web programming', },
+  { year: 2022, text: 'Started Studying Computer Science at Babcock University', },
+  { year: 2023, text: 'Participated in Campus Hackathon', },
 ];
